296fa1d0635ad9b800a0d32576c412640fabf0e7
张语嫣 <zhangyuyan@mail.ustc.edu.cn>
2026-09-09 19:10:45 -0700

    Restore pre-3D academic homepage

--- assets/js/scene.js
+++
@@ -1,9 +0,0 @@
-import {getSeason} from './season.js';
-import {createGarden as createLegacyGarden} from './legacy-scene.js';
-import {createBeautyGarden} from './beauty-scene.js';
-
-export async function createGarden(canvas, progress) {
-  return getSeason().id === 'autumn'
-    ? createBeautyGarden(canvas, progress)
-    : createLegacyGarden(canvas, progress);
-}
